c8f4725e77e5d285fee407dbaf74fefa12df733a
Benni <benni@karafi.at>
2021-01-13 19:04:49 +0100

    Erster Test für WebProject

--- index.php
+++ index.php
@@ -152,7 +152,6 @@ if (isset($_GET['delete'])) {
     $db->deleteUser($_GET["delete"]);
     // $deleteuser = "DELETE FROM user WHERE id = '".$_GET["delete"]."'";
     // $result = $mysqli->query($deleteuser);
-    //test
     echo "<script>window.location.href='index.php?menu=UserAdministration';</script>";
 }
 
